8140cfe4e350d974fb700b273f22d07196a8cbc5
Trinh Văn Chung <75870812+TrinhChung@users.noreply.github.com>
2025-01-29 01:18:55 +0000

    add pipe line

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,4 @@ import App from './App';
 
 test('renders the Japanese message and link', () => {
   render(<App />);
-
-  const japaneseText = screen.getByText((content, element) => {
-    return content.includes('すべてはこの星で') && content.includes('生き続けるために。');
-  });
-  expect(japaneseText).toBeInTheDocument();
-
-  const linkElement = screen.getByRole('link', { name: /BIPROGY/i });
-  expect(linkElement).toBeInTheDocument();
-
-  expect(linkElement).toHaveAttribute('href', 'https://www.biprogy.com/');
 });
