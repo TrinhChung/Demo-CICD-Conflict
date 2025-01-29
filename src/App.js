6de90b4c6ca997d9f8ef7ee21b7c46fbff6f2dd0
Chung370 <chungtv370@gmail.com>
2025-01-29 04:53:02 +0000

    step 1

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("Step 1")
-
-  console.log("Step 2")
-
-  console.log("Step 3")
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>step 3</p>
+        <p>step 2</p>
+        <p>step 1</p>
         <p>
           すべてはこの星で　生き続けるために。
         </p>
+        <p>step 2</p>
+        <p>step 1</p>
         <a
           className="App-link"
           href="https://www.biprogy.com/"
